76e40be7565f17664ab2d33d9b6f0e9cfbfb8386
ghbaq <1562073264@qq.com>
2019-12-29 22:24:46 +0800

    数据结构 queue

---
+++ src/main/scala/com/example/scala/chapter10/list/Exercise.scala
@@ -0,0 +1,11 @@
+package com.example.scala.chapter10.list
+
+/**
+  * @author : gaohui
+  * @Date : 2019/12/29 21:19
+  * @Description :   
+  *
+  */
+object Exercise {
+
+}
---
+++ src/main/scala/com/example/scala/chapter10/queue/Demo01.scala
@@ -0,0 +1,11 @@
+package com.example.scala.chapter10.queue
+
+/**
+  * @author : gaohui
+  * @Date : 2019/12/29 21:50
+  * @Description :   
+  *
+  */
+object Demo01 {
+
+}
