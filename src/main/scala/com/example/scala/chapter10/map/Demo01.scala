ef44b1ea3fd40bf268585f4cca4dab23bb9cd45a
ghbaq <1562073264@qq.com>
2020-01-02 22:34:59 +0800

    map & set

---
+++ src/main/scala/com/example/scala/chapter10/map/Demo01.scala
@@ -0,0 +1,11 @@
+package com.example.scala.chapter10.map
+
+/**
+  * @author : gaohui
+  * @Date : 2020/1/2 21:42
+  * @Description :   
+  *
+  */
+object Demo01 {
+
+}
---
+++ src/main/scala/com/example/scala/chapter10/set/Demo1.scala
@@ -0,0 +1,11 @@
+package com.example.scala.chapter10.set
+
+/**
+  * @author : gaohui
+  * @Date : 2020/1/2 22:30
+  * @Description :   
+  *
+  */
+object Demo1 {
+
+}
